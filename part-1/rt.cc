14d25ae4438112ad5ad20aff1fb2457443886019
Yamato Eguchi <yamatoe1227@csu.fullerton.edu>
2021-04-27 11:06:30 -0700

    this is my sky assignment

--- part-1/rt.cc
+++ part-1/rt.cc
@@ -1,3 +1,13 @@
+// Yamato Eguchi
+// CPSC-120-01
+// 2021-04-25
+// yamatoe1227@csu.fullerton.edu
+// @yamatoeguchi
+//
+// Lab 10-01
+//
+// "This is my rt.cc assignment"
+//
 
 #include <chrono>
 #include <cmath>
